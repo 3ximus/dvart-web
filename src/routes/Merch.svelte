a57033e753585708d851c2cfaebb0c34a0ee9718
louisdvart <82176837+louisdvart@users.noreply.github.com>
2024-10-07 17:22:37 +0100

    Update Merch.svelte

--- src/routes/Merch.svelte
+++ src/routes/Merch.svelte
@@ -9,13 +9,13 @@
 		<div class="scrolling-text">
 			<div class="scrolling-container">
 				<div class="scrolling-container__item">
-					SOLD OUT, DM FOR RESTOCK&nbsp;&nbsp;&nbsp;<it>"HOMEKIT" by LD</it>&nbsp;&nbsp;&nbsp;
+					sold out, dm for restock&nbsp;&nbsp;&nbsp;<it>"HOMEKIT" by LD</it>&nbsp;&nbsp;&nbsp;
 				</div>
 				<div class="scrolling-container__item">
-					SOLD OUT, DM FOR RESTOCK&nbsp;&nbsp;&nbsp;<it>"HOMEKIT" by LD</it>&nbsp;&nbsp;&nbsp;
+					sold out, dm for restock&nbsp;&nbsp;&nbsp;<it>"HOMEKIT" by LD</it>&nbsp;&nbsp;&nbsp;
 				</div>
 				<div class="scrolling-container__item">
-					SOLD OUT, DM FOR RESTOCK&nbsp;&nbsp;&nbsp;<it>""HOMEKIT" by LD"</it>&nbsp;&nbsp;&nbsp;
+					sold out, dm for restock&nbsp;&nbsp;&nbsp;<it>""HOMEKIT" by LD"</it>&nbsp;&nbsp;&nbsp;
 				</div>
 			</div>
 		</div>
